d8a9db27aa813aaba4f73655a6536e98c20bc23f
gzshell <sun.qinglong@qq.com>
2018-08-14 18:42:13 +0800

    success

--- main/main.js
+++ main/main.js
@@ -1,3 +1,6 @@
-module.exports = function main() {
+module.exports = function main(str) {
   // Write your code here
+  var arr = str.split('');
+  var sum = eval(arr.join("+"));
+  return sum ;
 };
